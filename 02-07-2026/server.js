b2b01f7dddf654c41292f148ccdbae10c6e4f0b9
achneerov <achneerov@gmail.com>
2026-07-02 18:44:53 -0400

    s

--- 02-07-2026/server.js
+++ 02-07-2026/server.js
@@ -1,11 +1,12 @@
-import { createServer } from 'node:http';
-const hostname = '127.0.0.1';
+import express from 'express';
+
+const app = express();
 const port = 3000;
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
