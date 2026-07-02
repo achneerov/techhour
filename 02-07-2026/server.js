95fa767d19634bb5793406d76a55941293c8e797
Berny-ft <berenis.fopa@gmail.com>
2026-07-02 19:06:43 -0400

    middleware

--- 02-07-2026/server.js
+++ 02-07-2026/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import bodyParser from 'body-parser/json';
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 
+app.post('/message',(req,res) => {
+    body = req.body
+
+})
 
 
 app.listen(port, () => {
